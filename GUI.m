e1250e5103cca329bfddd90a42d279d14e4eda99
pgpereyra <55006459+pgpereyra@users.noreply.github.com>
2019-09-11 12:32:29 -0500

    Add files via upload

---
+++ GUI.m
@@ -0,0 +1,128 @@
+function varargout = GUI(varargin)
+% GUI MATLAB code for GUI.fig
+%      GUI, by itself, creates a new GUI or raises the existing
+%      singleton*.
+%
+%      H = GUI returns the handle to a new GUI or the handle to
+%      the existing singleton*.
+%
+%      GUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in GUI.M with the given input arguments.
+%
+%      GUI('Property','Value',...) creates a new GUI or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before GUI_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to GUI_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help GUI
+
+% Last Modified by GUIDE v2.5 08-Sep-2019 00:23:28
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @GUI_OpeningFcn, ...
+                   'gui_OutputFcn',  @GUI_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before GUI is made visible.
+function GUI_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to GUI (see VARARGIN)
+
+% Choose default command line output for GUI
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes GUI wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = GUI_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+Data= evalin('base', 'Data');
+[coeff,D]=pca(Data.Spikes'); %Get the PCA of the data, you have to change the name of the field for the name of the variable
+plot(D(:,1),D(:,2),'.','Markersize',10);%plot the pca, every point is a frame (2componente)
+assignin('base', 'D', D)% asign the variable for the next function
+
+% --- Executes on button press in pushbutton2.
+function pushbutton2_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+Data= evalin('base', 'Data');
+[U,S,~] = svd(Data.Spikes');% Get the SDV
+D=U*S;% get the new representative matriz
+plot(D(:,1),D(:,2),'.','MarkerSize',10);%plot SDV, every point is a frame
+assignin('base', 'D', D) %asing the variable for the netx functions
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+D= evalin('base', 'D');
+eva = evalclusters(D,'kmeans','CalinskiHarabasz','KList',[1:6]); %evaluate the optimal cluster number
+yeastPC = D(:,1:2);%get the two components
+[clusters, centroid] = kmeans(yeastPC,eva.OptimalK);%calculate kmeans
+gscatter(yeastPC(:,1),yeastPC(:,2),clusters)%plot the cluster with differents colors
+
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+D= evalin('base', 'D');
+eva = evalclusters(D,'linkage','CalinskiHarabasz','KList',[1:6]);%evaluate the optimal cluster number
+gm = fitgmdist(D,eva.OptimalK,'RegularizationValue',0.05); %get Gaussian mixture distribution model
+idx = cluster(gm,D); %Construct agglomerative clusters from linkages
+gscatter(D(:,1),D(:,2),idx);%plot th clusters
+clc
+
+% --- Executes on button press in pushbutton5.
+function pushbutton5_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton5 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[filename pathname]=uigetfile({'*.mat'},'File Selector');
+command = sprintf('load(''%s'')', filename);
+Data=evalin('base', command);
+assignin('base', 'Data', Data)
